47dcfe632fc9b4435be05bf0a4c58089c5c3abea
joey1089 <josephvpeter7@gmail.com>
2022-07-12 22:29:13 -0400

    updated code with dict items

--- codesmith-challange.py
+++ codesmith-challange.py
@@ -4,13 +4,14 @@ CONST_NAMES = ['Mira', 'Royce', 'Kathie'];
 CONST_NUMBERS = ['3234958675', '9164059384', '4154958675']
 
 # // Write your code below this line
-for name in CONST_NAMES:
-    print(name)
-    if any(CONST_PHONEBOOK):
-        CONST_PHONEBOOK += name
-    else:
-        CONST_PHONEBOOK = name
+for name,numbers in zip(CONST_NAMES,CONST_NUMBERS):
+    CONST_PHONEBOOK[name] = numbers
 
+print(CONST_PHONEBOOK)
 
 
 # //Uncomment the lines below to test your code
+print(CONST_PHONEBOOK['Mira'])
+print(CONST_PHONEBOOK['Royce'])
+print(CONST_PHONEBOOK['Kathie'])
+
