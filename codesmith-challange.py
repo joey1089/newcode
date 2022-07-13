04c1778cd6f08e7b2e8a61dc00b223118fb5cbfc
joey1089 <josephvpeter7@gmail.com>
2022-07-12 21:46:10 -0400

    codesmith challenge

---
+++ codesmith-challange.py
@@ -0,0 +1,11 @@
+// If you're having trouble, use lines 12-14 for expected inputs and outputs
+const phoneBook = {};
+const names = ['Mira', 'Royce', 'Kathie'];
+const numbers = ['3234958675', '9164059384', '4154958675']
+
+// Write your code below this line
+for name in names:
+    print(name)
+
+
+//Uncomment the lines below to test your code
