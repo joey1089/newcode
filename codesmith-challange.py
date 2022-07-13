f2601b2a8e4e0eabe17cec33dc8a1e9dc9adc030
joey1089 <josephvpeter7@gmail.com>
2022-07-12 22:04:45 -0400

    deleted imports

--- codesmith-challange.py
+++ codesmith-challange.py
@@ -1,8 +1,4 @@
 # // If you're having trouble, use lines 12-14 for expected inputs and outputs
-from asyncio.windows_events import NULL
-from queue import Empty
-
-
 CONST_PHONEBOOK = {};
 CONST_NAMES = ['Mira', 'Royce', 'Kathie'];
 CONST_NUMBERS = ['3234958675', '9164059384', '4154958675']
