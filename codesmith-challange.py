045ec784f9c255e68c0925176cfd1d633f212d61
joey1089 <josephvpeter7@gmail.com>
2022-07-12 21:59:05 -0400

    converted javascript to python and added forloop

--- codesmith-challange.py
+++ codesmith-challange.py
@@ -1,11 +1,20 @@
-// If you're having trouble, use lines 12-14 for expected inputs and outputs
-const phoneBook = {};
-const names = ['Mira', 'Royce', 'Kathie'];
-const numbers = ['3234958675', '9164059384', '4154958675']
+# // If you're having trouble, use lines 12-14 for expected inputs and outputs
+from asyncio.windows_events import NULL
+from queue import Empty
 
-// Write your code below this line
-for name in names:
+
+CONST_PHONEBOOK = {};
+CONST_NAMES = ['Mira', 'Royce', 'Kathie'];
+CONST_NUMBERS = ['3234958675', '9164059384', '4154958675']
+
+# // Write your code below this line
+for name in CONST_NAMES:
     print(name)
+    if CONST_PHONEBOOK is Empty:
+        CONST_PHONEBOOK = name
+    else:
+        CONST_PHONEBOOK += name
 
 
-//Uncomment the lines below to test your code
+
+# //Uncomment the lines below to test your code
